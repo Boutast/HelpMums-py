f227c4fdd617157bbd955c0772db58f0ff07a2a3
Arthur <arthur.costil@epitech.eu>
2020-04-26 13:46:25 +0200

    migrate database horror show

--- helpmoms/emergency/models.py
+++ helpmoms/emergency/models.py
@@ -3,13 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 class Emergency(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
+    
     def __str__(self):
         return self.text
